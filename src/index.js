ca8a4610f4ee5214a759440c01ba7057bbf79033
Krystian Greblowski <krystian.greblowski@gmail.com>
2023-12-04 20:51:44 +0100

    unnecessary code deleted

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ import { theme } from "./theme";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <ThemeProvider theme={theme}>
-    {" "}
     <React.StrictMode>
       <GlobalStyles />
       <App />
