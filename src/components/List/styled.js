a972c2ae9cbbcc3fa64b4c3f5a643467c4c28c81
Krystian Greblowski <krystian.greblowski@gmail.com>
2023-12-09 15:28:53 +0100

    styles to List component added

---
+++ src/components/List/styled.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  width: 1216px;
+  padding: 32px;
+  margin: auto;
+  margin-top: 63px;
+  background-color: ${({ theme }) => theme.color.white};
+  box-shadow: 0px 16px 58px 0px rgba(9, 10, 51, 0.03),
+    0px -2px 50px 0px rgba(9, 10, 51, 0.02);
+`;
+
+export const Title = styled.h2`
+  font-size: 30px;
+  font-style: normal;
+  font-weight: 900;
+  line-height: normal;
+  letter-spacing: 1.5px;
+  border-bottom: 1px solid rgba(209, 213, 218, 0.3);
+  padding-bottom: 15px;
+  margin: 0;
+`;
