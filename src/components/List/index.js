5b4d968e8cf3247657a7208287a328fbfae5c211
Krystian Greblowski <krystian.greblowski@gmail.com>
2023-12-09 15:28:34 +0100

    List component added

---
+++ src/components/List/index.js
@@ -0,0 +1,21 @@
+import { Wrapper, Title } from "./styled";
+
+const List = () => {
+  return (
+    <Wrapper>
+      <Title>My skillset includes</Title>
+      <ul>
+        <li>test 1</li>
+        <li>test 2</li>
+        <li>test 3</li>
+        <li>test 4</li>
+        <li>test 5</li>
+        <li>test 6</li>
+        <li>test 7</li>
+        <li>test 8</li>
+      </ul>
+    </Wrapper>
+  );
+};
+
+export default List;
