4417ad0e687f5987fcfb8ce358450a894fbcf964
Krystian Greblowski <krystian.greblowski@gmail.com>
2023-12-16 14:25:07 +0100

    making List component reusabled

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import { Container } from "./components/Container";
-import { Tile } from "./components/Tile";
 import List from "./components/List";
+import { skillsetList } from "./data/skillsetList";
+import { learningList } from "./data/learningList";
+import { ReactComponent as ToolIcon } from "./images/tool.svg";
+import { ReactComponent as RocketIcon } from "./images/rocket.svg";
 
 function App() {
   return (
     <Container>
-      <Tile>
-        <List />
-      </Tile>
+      <List
+        titleText={"My skillset includes"}
+        titleIcon={<ToolIcon />}
+        listItems={skillsetList}
+      />
+      <List
+        titleText={"What I want to learn next"}
+        titleIcon={<RocketIcon />}
+        listItems={learningList}
+      />
     </Container>
   );
 }
--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,3 +1,4 @@
+import { nanoid } from "nanoid";
 import {
   Wrapper,
   HeaderTitle,
@@ -6,34 +7,18 @@ import {
   StyledList,
   ListItem,
 } from "./styled";
-import { ReactComponent as ToolIcon } from "../../images/tool.svg";
 
-const List = () => {
+const List = ({ titleText, titleIcon, listItems }) => {
   return (
     <Wrapper>
       <HeaderTitle>
-        <TitleText>My skillset includes</TitleText>
-        <TitleIcon>
-          <ToolIcon />
-        </TitleIcon>
+        <TitleText>{titleText}</TitleText>
+        <TitleIcon>{titleIcon}</TitleIcon>
       </HeaderTitle>
       <StyledList>
-        <ListItem>test 1</ListItem>
-        <ListItem>test 2</ListItem>
-        <ListItem>test 3</ListItem>
-        <ListItem>test 4</ListItem>
-        <ListItem>test 5</ListItem>
-        <ListItem>test 6</ListItem>
-        <ListItem>test 7</ListItem>
-        <ListItem>test 8</ListItem>
-        <ListItem>test 9</ListItem>
-        <ListItem>test 10</ListItem>
-        <ListItem>test 11</ListItem>
-        <ListItem>test 12</ListItem>
-        <ListItem>test 13</ListItem>
-        <ListItem>test 14</ListItem>
-        <ListItem>test 15</ListItem>
-        <ListItem>test 16</ListItem>
+        {listItems.map((listItem) => (
+          <ListItem key={nanoid()}>{listItem}</ListItem>
+        ))}
       </StyledList>
     </Wrapper>
   );
