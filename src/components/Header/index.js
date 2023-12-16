092bbda691f1093d2a39218e13ca89d48ccd4f06
Krystian Greblowski <krystian.greblowski@gmail.com>
2023-12-16 19:09:01 +0100

    Wrapper created and grid added

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,20 @@
 import myPhoto from "../../images/header-image-test.png";
-import { HeaderImage } from "./styled";
+import { Wrapper, HeaderImage } from "./styled";
 
 const Header = () => {
   return (
-    <>
+    <Wrapper>
       <HeaderImage src={myPhoto} alt="Krystian Gręblowski" />
-    </>
+      <div>
+        <p>This is</p>
+        <h1>Krystian Greblowski</h1>
+        <p>
+          I'm a passionate Frontend Developer in love with React, currently
+          looking for new job opportunities.
+        </p>
+        <button>Hire me</button>
+      </div>
+    </Wrapper>
   );
 };
 
--- src/components/Header/styled.js
+++ src/components/Header/styled.js
@@ -1,5 +1,10 @@
 import styled from "styled-components";
 
+export const Wrapper = styled.div`
+  display: grid;
+  grid-template-columns: auto 1fr;
+`;
+
 export const HeaderImage = styled.img`
   width: 398px;
   height: 398px;
