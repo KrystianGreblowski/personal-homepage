252ce8a65c6aab91d99d8b6f5e6b94610223d887
Krystian Greblowski <krystian.greblowski@gmail.com>
2023-12-25 14:14:12 +0100

    grid added to Loading

--- src/components/Portfolio/Loading/styled.js
+++ src/components/Portfolio/Loading/styled.js
@@ -2,6 +2,9 @@ import styled from "styled-components";
 
 export const Wrapper = styled.div`
   text-align: center;
+  display: grid;
+  grid-template-columns: 1fr;
+  justify-items: center;
   margin: 64px 0 20px 0;
 `;
 
@@ -16,7 +19,6 @@ export const Text = styled.p`
 `;
 
 export const Icon = styled.div`
-  display: inline-block;
   width: 160px;
   height: 160px;
 `;
