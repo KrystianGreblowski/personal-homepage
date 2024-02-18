aed4a7802b2b9a6172a767935cbb4912990ae601
Krystian Greblowski <krystian.greblowski@gmail.com>
2024-02-18 18:24:53 +0100

    max-width changed and border added

--- src/components/Header/MailButton/styled.js
+++ src/components/Header/MailButton/styled.js
@@ -3,11 +3,12 @@ import styled from "styled-components";
 export const Button = styled.a`
   display: grid;
   grid-template-columns: auto auto;
-  max-width: 159px;
+  max-width: 161px;
   grid-gap: 16px;
   padding: 12px 16px;
   text-decoration: none;
   border-radius: 4px;
+  border: 1px solid rgba(209, 213, 218, 0.3);
   background-color: ${({ theme }) => theme.colors.background.mailButton};
   margin-top: 32px;
   margin-bottom: 83px;
