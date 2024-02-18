d719cf09f008c7c75b4a6aef36696aff2a200bc9
Krystian Greblowski <krystian.greblowski@gmail.com>
2024-02-18 18:25:21 +0100

    window.open removed and link added

--- src/components/Portfolio/Error/ErrorButton/index.js
+++ src/components/Portfolio/Error/ErrorButton/index.js
@@ -1,10 +1,11 @@
 import { Button } from "./styled";
 
 const ErrorButton = ({ url }) => {
-  const errorHandler = () => {
-    window.open(url, "_blank");
-  };
-  return <Button onClick={errorHandler}>Go to GitHub</Button>;
+  return (
+    <Button href={url} target="_blank">
+      Go to GitHub
+    </Button>
+  );
 };
 
 export default ErrorButton;
--- src/components/Portfolio/Error/ErrorButton/styled.js
+++ src/components/Portfolio/Error/ErrorButton/styled.js
@@ -1,12 +1,12 @@
 import styled from "styled-components";
 
-export const Button = styled.button`
+export const Button = styled.a`
   color: ${({ theme }) => theme.colors.font.errorButton};
   background-color: ${({ theme }) => theme.colors.background.errorButton};
   padding: 12px 16px;
   border-radius: 4px;
   border: 1px solid var(--semi-grey, rgba(209, 213, 218, 0.1));
-  text-align: center;
+  text-decoration: none;
   font-size: 20.058px;
   font-style: normal;
   font-weight: 600;
