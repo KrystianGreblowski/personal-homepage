a51d86a99501a70ded7e5b90a7e4e17605751fb7
Krystian Greblowski <krystian.greblowski@gmail.com>
2024-04-20 16:50:15 +0200

    List responsive styles corrected

--- src/components/List/styled.js
+++ src/components/List/styled.js
@@ -3,14 +3,19 @@ import styled from "styled-components";
 export const Wrapper = styled.div`
   padding: 32px;
   margin: auto;
-  margin-top: 72px;
+  margin-top: 64px;
   background-color: ${({ theme }) => theme.colors.background.listWrapper};
   box-shadow: 0px 16px 58px 0px rgba(9, 10, 51, 0.03),
     0px -2px 50px 0px rgba(9, 10, 51, 0.02);
 
+  @media (max-width: ${({ theme }) => theme.breakpoints.large}px) {
+    margin-top: 32px;
+    padding: 24px;
+  }
+
   @media (max-width: ${({ theme }) => theme.breakpoints.medium}px) {
+    margin-top: 24px;
     padding: 16px;
-    margin-top: 50px;
   }
 `;
 
@@ -19,10 +24,14 @@ export const HeaderTitle = styled.div`
   grid-template-columns: auto 1fr;
   grid-gap: 16px;
   border-bottom: 1px solid ${({ theme }) => theme.colors.border.listHeaderTitle};
+  align-items: baseline;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.large}px) {
+    grid-gap: 10px;
+  }
 
   @media (max-width: ${({ theme }) => theme.breakpoints.medium}px) {
-    grid-gap: 12px;
-    align-items: baseline;
+    grid-gap: 6px;
   }
 `;
 
@@ -36,6 +45,11 @@ export const Title = styled.h2`
   margin: 0;
   margin-bottom: 18px;
 
+  @media (max-width: ${({ theme }) => theme.breakpoints.large}px) {
+    font-size: 26px;
+    margin-bottom: 14px;
+  }
+
   @media (max-width: ${({ theme }) => theme.breakpoints.medium}px) {
     margin-bottom: 12px;
     font-size: 18px;
@@ -45,12 +59,15 @@ export const Title = styled.h2`
 
 export const Icon = styled.div`
   width: 27px;
-  height: 32px;
   margin-top: 6px;
 
+  @media (max-width: ${({ theme }) => theme.breakpoints.large}px) {
+    width: 22px;
+    margin-top: 4px;
+  }
+
   @media (max-width: ${({ theme }) => theme.breakpoints.medium}px) {
-    width: 18px;
-    height: 21px;
+    width: 15px;
   }
 `;
 
@@ -70,6 +87,11 @@ export const StyledList = styled.ul`
 
   @media (max-width: ${({ theme }) => theme.breakpoints.extraLarge}px) {
     grid-template-columns: repeat(2, 1fr);
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.large}px) {
+    margin-top: 24px;
+    font-size: 16px;
   }
 
   @media (max-width: ${({ theme }) => theme.breakpoints.medium}px) {
